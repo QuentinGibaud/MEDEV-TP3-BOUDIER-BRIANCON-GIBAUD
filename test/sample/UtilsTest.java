38290f00e9c58679e85375f87cc43de329b7a249
QuentinGibaud <quentgib@gmail.com>
2017-11-23 11:14:53 +0100

    Ajout d'un test et src

---
+++ test/sample/UtilsTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Quentin GIBAUD
+ */
+public class UtilsTest {
+    
+    public UtilsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of concatWords method, of class Utils.
+     */
+    @Test
+    public void testConcatWords() {
+        System.out.println("concatWords");
+        String[] words = null;
+        String expResult = "";
+        String result = Utils.concatWords(words);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of computeFactorial method, of class Utils.
+     */
+    @Test
+    public void testComputeFactorial() {
+        System.out.println("computeFactorial");
+        int number = 0;
+        String expResult = "";
+        String result = Utils.computeFactorial(number);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
